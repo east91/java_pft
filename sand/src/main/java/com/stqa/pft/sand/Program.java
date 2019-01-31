7971e75f793b755839d9372320b1ffeb34e8a9c5
Boris Kliachenkov <bkliachenkov@Carnegietechnologies.com>
2019-01-30 21:44:39 -0600

    added 3 classes

--- sand/src/main/java/com/stqa/pft/sand/Program.java
+++ sand/src/main/java/com/stqa/pft/sand/Program.java
@@ -1,8 +1,29 @@
 package com.stqa.pft.sand;
 
+import java.awt.*;
+
+import static java.lang.Math.sqrt;
+
 public class Program {
 
+
   public static void main(String[] args) {
-System.out.println("World!");
+
+
+    Square s = new Square(5);
+    System.out.println("Square of square " + s.l + " = " + s.area());
+
+    Rectangle r = new Rectangle(4,6);
+    System.out.println("Square of rectangle " + r.a + " and " + r.b + " = " + r.area());
+
+    Point p1 = new Point(10, 20);
+    Point p2 = new Point(30, 40);
+
+    System.out.println("Distance between point 1 [" + p1.x + "," + p1.y + "] and point 2 [" + p2.x + ", " + p2.y + "] equals " + distance(p1, p2));
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return sqrt(p1.x * p2.x + p1.y * p2.y);
   }
 }
+
