cf1a48c397f97e66452bff24af7a1498d67048dd
Boris Kliachenkov <bkliachenkov@Carnegietechnologies.com>
2019-02-03 18:15:28 -0600

    added class ContactHelper

---
+++ addressbook-web-tests/src/test/java/com/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -0,0 +1,46 @@
+package com.stqa.pft.addressbook.appmanager;
+
+import com.stqa.pft.addressbook.model.ContactData;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ContactHelper {
+  FirefoxDriver wd;
+
+  public void selectContact() {
+    selectContact();
+  }
+
+  public void editContact() {
+    wd.findElement(By.xpath("//*[@id=\"maintable\"]/tbody/tr[2]/td[8]/a/img")).click();
+    JavascriptExecutor jse = (JavascriptExecutor) wd;
+    jse.executeScript("window.scrollBy(0,350)", "");
+  }
+
+  public void deleteSelectedContacts() {
+    wd.findElement(By.xpath("//*[@id=\"content\"]/form[2]/input[2]")).click();
+  }
+
+  public void submitContactCreation() {
+    submitContactCreation();
+  }
+
+  public void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+    wd.findElement(By.name("mobile")).click();
+    wd.findElement(By.name("mobile")).clear();
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+  }
+}
