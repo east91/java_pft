dcdaf8dc33dce08340de0490e4a414ce72744091
Boris Kliachenkov <bkliachenkov@Carnegietechnologies.com>
2019-02-11 19:48:37 -0600

    Before and After accepts List

--- addressbook-web-tests/src/test/java/com/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/com/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -4,21 +4,24 @@ import com.stqa.pft.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class GroupDeletionTests extends TestBase {
 
   @Test
   public void testGroupDeletion() throws InterruptedException {
     app.getNavigationHelper().gotoGroupPage();
     Thread.sleep(100);
-    int before = app.getGroupHelper().getGroupCount();
+
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before - 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() - 1);
 
 
   }
--- addressbook-web-tests/src/test/java/com/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/com/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,22 +4,25 @@ import com.stqa.pft.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class GroupModificationTests extends TestBase {
 
   @Test
   public void testGroupModification() throws InterruptedException {
     app.getNavigationHelper().gotoGroupPage();
     Thread.sleep(100);
-    int before = app.getGroupHelper().getGroupCount();
+
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test1", null, null));
     }
-    app.getGroupHelper().selectGroup(before - 1);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().EditSelectedGroups();
     app.getGroupHelper().fillGroupForm(new GroupData("edited test1", "edited test2", "edited test3"));
     app.getGroupHelper().submitUpdateGroupCreation();
     app.getGroupHelper().returnToGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size());
   }
 }
